7f41b94b4994f8b2c2990f081a65a6f8e993c720
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-15 10:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+
+let form = document.querySelector('form');
+let input1 = document.querySelector('#age');
+let input2 = document.querySelector('#name');
+let btn = document.querySelector('button');
+let container = document.querySelector('container');
+
+btn.addEventListener('submit' => {
+	e.preventDafaul();
+	let input1Value = input1.value;
+	let input2Value = input2.value;
+
+	if(input1Value.length == 0 || input2Value === undefined){
+		alert('Please enter valid details.');
+	}
+
+	new Promise((resolve,reject) => {
+		let flag = (input1Value > 18)
+		resolve(flag);
+	})
+}).then((flag)=>{
+	setTimeout(()=> {
+		container.innerText = `Welcome, . You can vote.`
+	},4000)
+})
+.catch((error)=>{
+	setTimeout(()=>{
+		container.innerText = `Oh sorry . You aren't old enough.`
+	},4000)
+})
