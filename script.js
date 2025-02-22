7b0a66a313bfa1a7144c5cde37bfb63431b08944
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-22 10:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ let ageBox = document.getElementById('age');  // Get the age input element by ID
 let nameBox = document.getElementById('name');  // Get the name input element by ID
 
 
-    // Get input values
-    let age = ageBox.value;  // Convert age input to number
-    let name = nameBox.value;   // Get name value
-
    
 
 // Add event listener to form submit
 form.addEventListener("submit", (e) => {
+	
     e.preventDefault();  // Prevent default form submission behavior
+	// Get input values
+	
+    let age = +ageBox.value;  // Convert age input to number
+    let name = nameBox.value;  // Get name value
 
-	 // Validate if the inputs are empty
-    if (!age || !name) {
+    // Validate if the inputs are empty
+    if (ageBox.value == '' || name.length == 0) {
         alert(`Please enter valid details`);
-    }else{
+    } else{
 		
     // Promise for age check
     new Promise((resolve, reject) => {
